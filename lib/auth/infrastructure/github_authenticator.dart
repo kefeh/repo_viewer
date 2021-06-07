69558f16e70899fd2a3f91b97070a4c1e7dbd176
kefeh <kefehcollins@gmail.com>
2021-06-07 12:01:43 +0100

    adding the get for signed credentials

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,5 +1,70 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/services.dart';
 import 'package:oauth2/oauth2.dart';
+import 'package:repo_viewer/auth/dormain/auth_failure.dart';
+import 'package:repo_viewer/auth/infrastructure/credential_storage/credential_storage.dart';
+import 'package:http/http.dart' as http;
+
+class GithubOAuthHttpClient extends http.BaseClient {
+  final httpClient = http.Client();
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Accept'] = 'application/json';
+    return httpClient.send(request);
+  }
+}
 
 class GithubAuthenticator {
-  Future<Credentials> getSignedInCredentials() {}
+  final CredentialStorage _credentialStorage;
+
+  GithubAuthenticator(this._credentialStorage);
+
+  static const clientId = 'caf56c4ead1731927884';
+  static const clientSecrete = 'b8816e5b778dd619213a3cb7563511d3b9d108cf';
+  static const scopes = ['read:user', 'repo'];
+  static final authorizationEndpoint =
+      Uri.parse('https://github.com/login/oauth/authorize');
+  static final tokenEndpoint =
+      Uri.parse('https://github.com/login/oauth/access_token');
+  static final redirectEndpoint = Uri.parse('http://localhost:3000/callback');
+
+  Future<Credentials?> getSignedInCredentials() async {
+    try {
+      final storedCredentials = await _credentialStorage.read();
+      if (storedCredentials!.canRefresh && storedCredentials.isExpired) {
+        //TODO: refresh the token
+      }
+      return storedCredentials;
+    } on PlatformException {
+      return null;
+    }
+  }
+
+  Future<bool> isSignedIn() =>
+      getSignedInCredentials().then((credentials) => credentials != null);
+
+  AuthorizationCodeGrant createGrant() {
+    return AuthorizationCodeGrant(
+        clientId, authorizationEndpoint, tokenEndpoint,
+        secret: clientSecrete, httpClient: GithubOAuthHttpClient());
+  }
+
+  Uri getAuthorizationUri(AuthorizationCodeGrant grant) {
+    return grant.getAuthorizationUrl(redirectEndpoint, scopes: scopes);
+  }
+
+  Future<Either<AuthFailure, Unit>> handleAuthorizationFailure(
+      AuthorizationCodeGrant grant, Map<String, String> queryParams) async {
+    try {
+      final httpClient = await grant.handleAuthorizationResponse(queryParams);
+      await _credentialStorage.save(httpClient.credentials);
+      return right(unit);
+    } on FormatException {
+      return left(const AuthFailure.server());
+    } on AuthorizationException catch (e) {
+      return left(AuthFailure.server('${e.error}: ${e.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
 }
