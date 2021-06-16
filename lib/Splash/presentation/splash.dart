8e9eae7724a0629ed8546878908c0c702df0d496
kefeh <kefehcollins@gmail.com>
2021-06-16 11:17:57 +0100

    initialising the first few presentation pages

---
+++ lib/Splash/presentation/splash.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splasj extends StatelessWidget {
+  const Splasj({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
