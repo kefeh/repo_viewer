d6ae43e781129409bf9dc71ecbb39666502765a8
kefeh <kefehcollins@gmail.com>
2021-06-22 14:03:10 +0100

    made changes

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
+
+final inititializationProvider = FutureProvider((ref) async {
+  final authnotifier = ref.read(authNotifierProvider.notifier);
+  await authnotifier.checkAndUpdateAuthState();
+});
 
 class AppWidget extends StatelessWidget {
   final appRouter = AppRouter();
