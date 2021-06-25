6650f18326505f50be2690a8996753679847bc2d
kefeh <kefehcollins@gmail.com>
2021-06-25 16:50:09 +0100

    adds the code that implements the webview for signin

--- lib/auth/presentation/authorization_page.dart
+++ lib/auth/presentation/authorization_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class AuthorizationPage extends StatefulWidget {
   final Uri authorizationUri;
@@ -16,6 +18,25 @@ class AuthorizationPage extends StatefulWidget {
 class _AuthorizationPageState extends State<AuthorizationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: widget.authorizationUri.toString(),
+          onWebViewCreated: (controller) {
+            controller.clearCache();
+            CookieManager().clearCookies();
+          },
+          navigationDelegate: (navReq) {
+            if (navReq.url
+                .startsWith(GithubAuthenticator.redirectEndpoint.toString())) {
+              widget.onAuthorizationCodeRedirectAttempt(Uri.parse(navReq.url));
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      ),
+    );
   }
 }
