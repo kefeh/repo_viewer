4e85a4151eada6c433cf578c70cbc6eeb1296b2c
kefeh <kefehcollins@gmail.com>
2021-09-21 17:00:25 +0100

    starting with the data transfer objects

---
+++ lib/github/repos/core/infrastructure/user_dto.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_dto.freezed.dart';
+
+@freezed
+class UserDTO with _$UserDTO {
+  const UserDTO._();
+  const factory UserDTO({
+    required String name,
+    required String avatarUrl,
+  }) = _UserDTO;
+}
