8308fcbf9874a04f6132f3c2bd08d69459a8f2ef
kefeh <kefehcollins@gmail.com>
2021-09-23 15:39:27 +0100

    GithubRepoDTO

---
+++ lib/github/repos/core/infrastructure/github_repo_dto.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/github/repos/core/infrastructure/user_dto.dart';
+
+part 'github_repo_dto.freezed.dart';
+part 'github_repo_dto.g.dart';
+
+String _descriptionFromJson(Object? json) {
+  return (json as String?) ?? '';
+}
+
+@freezed
+class GithubRepoDTO with _$GithubRepoDTO {
+  const GithubRepoDTO._();
+  const factory GithubRepoDTO({
+    required UserDTO owner,
+    required String name,
+    @JsonKey(fromJson: _descriptionFromJson) required String description,
+    @JsonKey(name: "stargazers_count") required int stargazersCount,
+  }) = _GithubRepoDTO;
+
+  factory GithubRepoDTO.fromJson(Map<String, dynamic> json) =>
+      _$GithubRepoDTOFromJson(json);
+}
