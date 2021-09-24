b94d97dca60cd59a93883bef0755608217c4b8b1
kefeh <kefehcollins@gmail.com>
2021-09-24 06:00:16 +0100

    setting up the sembast database class

--- lib/github/repos/starred_repo/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repo/infrastructure/starred_repos_remote_service.dart
@@ -3,7 +3,7 @@ import 'package:repo_viewer/core/infrastructure/remote_response.dart';
 import 'package:repo_viewer/github/repos/core/infrastructure/github_repo_dto.dart';
 
 class StarredReposRemoteService {
-  final Dio _dio;
+  final Dio _dio = Dio();
 
   Future<RemoteResponse<List<GithubRepoDTO>>> getStarredRepos(int page) async {}
 }
