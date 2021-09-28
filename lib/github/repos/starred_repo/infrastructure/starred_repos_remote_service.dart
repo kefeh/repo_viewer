7001d9a88e87f95a12173252f46ac250836d8340
kefeh <kefehcollins@gmail.com>
2021-09-28 11:31:08 +0100

    Caching the gihub headers

--- lib/github/repos/starred_repo/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repo/infrastructure/starred_repos_remote_service.dart
@@ -1,9 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 
 class StarredReposRemoteService {
   final Dio _dio = Dio();
+  final GithubHeadersCache _headersCache;
+
+  StarredReposRemoteService(this._headersCache);
 
   Future<RemoteResponse<List<GithubRepoDTO>>> getStarredRepos(int page) async {}
 }
