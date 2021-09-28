c2f7aade9b5b41016b72d1e59efdf35209ab2123
kefeh <kefehcollins@gmail.com>
2021-09-28 11:15:10 +0100

    making slight changes

--- lib/github/repos/starred_repo/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repo/infrastructure/starred_repos_remote_service.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:repo_viewer/core/infrastructure/remote_response.dart';
-import 'package:repo_viewer/github/repos/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 
 class StarredReposRemoteService {
   final Dio _dio = Dio();
