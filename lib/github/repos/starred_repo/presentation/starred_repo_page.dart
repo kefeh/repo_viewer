cfbf0ce22cee569d3afadc74cac35dac86f0f172
kefeh <kefehcollins@gmail.com>
2021-11-01 17:28:12 +0100

    Initial Creation of the PaginatedReposListView

--- lib/github/repos/starred_repo/presentation/starred_repo_page.dart
+++ lib/github/repos/starred_repo/presentation/starred_repo_page.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:repo_viewer/auth/shared/providers.dart';
+import 'package:repo_viewer/github/core/shared/providers.dart';
 
-class StarredReposPage extends ConsumerWidget {
+class StarredReposPage extends StatefulWidget {
   const StarredReposPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetReference ref) {
+  State<StarredReposPage> createState() => _StarredReposPageState();
+}
+
+class _StarredReposPageState extends State<StarredReposPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() {
+      context
+          .read(starredReposNotifierProvider.notifier)
+          .getNextStarredReposPage();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: ElevatedButton(
+      appBar: AppBar(
+        title: const Text('Starred Repos'),
+        actions: [
+          IconButton(
             onPressed: () {
               context.read(authNotifierProvider.notifier).signOut();
             },
-            child: const Text('sign out'),
+            icon: const Icon(MdiIcons.logoutVariant),
           ),
-        ),
+        ],
       ),
+      body: PaginatedReposListView(),
     );
+  }
+}
+
+class PaginatedReposListView extends StatelessWidget {
+  const PaginatedReposListView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
   }
 }
