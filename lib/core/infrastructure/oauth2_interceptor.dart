d7f1eb39ae67f5b237b70161bbfcae118ac930be
kefeh <kefehcollins@gmail.com>
2021-11-01 11:54:58 +0100

    OAuth2Interceptor Part 2

--- lib/core/infrastructure/oauth2_interceptor.dart
+++ lib/core/infrastructure/oauth2_interceptor.dart
@@ -1,10 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
 import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
 
 class OAuth2Interceptor extends Interceptor {
   final GithubAuthenticator _authenticator;
+  final Dio _dio;
+  final AuthNotifier _authNotifier;
 
-  OAuth2Interceptor(this._authenticator);
+  OAuth2Interceptor(this._authenticator, this._dio, this._authNotifier);
 
   @override
   Future<void> onRequest(
@@ -31,6 +34,13 @@ class OAuth2Interceptor extends Interceptor {
       credentials != null && credentials.canRefresh
           ? _authenticator.refresh(credentials)
           : await _authenticator.clearCredentialsStorage();
+
+      _authNotifier.checkUpdateAuthStatus();
+      final refreshCredentials = await _authenticator.getSignedInCredentials();
+      if (refreshCredentials != null) {
+        handler.resolve(await _dio.fetch(err.requestOptions
+          ..headers['Authorization'] = 'Bearer $refreshCredentials'));
+      }
     }
   }
 }
