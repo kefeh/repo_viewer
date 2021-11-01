336acc8b60089b4daa00dcb3a946dcf70f80bf8d
kefeh <kefehcollins@gmail.com>
2021-11-01 06:51:11 +0100

    StarredReposRepository Implementation Part 2 - Local Service

--- lib/github/repos/starred_repo/infrastructure/starred_repos_repository.dart
+++ lib/github/repos/starred_repo/infrastructure/starred_repos_repository.dart
@@ -4,38 +4,43 @@ import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 import 'package:repo_viewer/github/repos/core/dormain/github_failure.dart';
 import 'package:repo_viewer/github/repos/core/dormain/github_repo.dart';
+import 'package:repo_viewer/github/repos/starred_repo/infrastructure/starred_repos_local_service.dart';
 import 'package:repo_viewer/github/repos/starred_repo/infrastructure/starred_repos_remote_service.dart';
 
 class StarredReposRepository {
   final StarredReposRemoteService _remoteService;
+  final StarredReposLocalService _localService;
 
-  StarredReposRepository(this._remoteService);
+  StarredReposRepository(this._remoteService, this._localService);
 
   Future<Either<GithubFailure, Fresh<List<GithubRepo>>>> getStarredReposPage(
     int page,
   ) async {
     try {
-      final remote_page = await _remoteService.getStarredRepos(page);
-      return right(remote_page.when(
-
-          // Todo Local service
-          noConnection: (maxPage) => Fresh.no(
-                [],
-                isNextPageAvailable: page < maxPage,
-              ),
-
-          // Todo Local service
-          notModified: (maxPage) => Fresh.yes(
-                [],
-                isNextPageAvailable: page < maxPage,
-              ),
-          withNewData: (data, maxPage) {
-            // Todo Local service
+      final remotePage = await _remoteService.getStarredRepos(page);
+      return right(
+        await remotePage.when(
+          noConnection: (maxPage) async => Fresh.no(
+            await _localService.getPage(page).then(
+                  (value) => value.toDomain(),
+                ),
+            isNextPageAvailable: page < maxPage,
+          ),
+          notModified: (maxPage) async => Fresh.yes(
+            await _localService.getPage(page).then(
+                  (value) => value.toDomain(),
+                ),
+            isNextPageAvailable: page < maxPage,
+          ),
+          withNewData: (data, maxPage) async {
+            _localService.upsertPage(data, page);
             return Fresh.yes(
               data.toDomain(),
               isNextPageAvailable: page < maxPage,
             );
-          }));
+          },
+        ),
+      );
     } on RestApiException catch (e) {
       return left(GithubFailure.api(erroCode: e.errorCode));
     }
