5f02dba429eefdb13bf87515816b858a18e351fd
kefeh <kefehcollins@gmail.com>
2021-11-01 06:32:32 +0100

    StarredReposLocalService - Reading a Page

--- lib/github/repos/starred_repo/infrastructure/starred_repos_local_service.dart
+++ lib/github/repos/starred_repo/infrastructure/starred_repos_local_service.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:repo_viewer/core/infrastructure/sembast_database.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
 import 'package:sembast/sembast.dart';
 
 class StarredReposLocalService {
@@ -12,10 +13,22 @@ class StarredReposLocalService {
   Future<void> upsertPage(List<GithubRepoDTO> dtos, int page) async {
     final sembastPage = page - 1;
     await _store
-        .records(dtos.mapIndexed((index, _) => index + 3 * sembastPage))
+        .records(dtos.mapIndexed(
+            (index, _) => index + PaginationConfig.itemsPerPage * sembastPage))
         .put(
           _sembastDatabase.instance,
           dtos.map((e) => e.toJson()).toList(),
         );
+  }
+
+  Future<List<GithubRepoDTO>> getPage(int page) async {
+    final sembastPage = page - 1;
+
+    final records = await _store.find(_sembastDatabase.instance,
+        finder: Finder(
+          limit: PaginationConfig.itemsPerPage,
+          offset: PaginationConfig.itemsPerPage * sembastPage,
+        ));
+    return records.map((e) => GithubRepoDTO.fromJson(e.value)).toList();
   }
 }
